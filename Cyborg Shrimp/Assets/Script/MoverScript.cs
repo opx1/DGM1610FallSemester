ff067c7ef48ee05ba6bdca81e38e1cc8b5fb2ac8
Natalie Rust <10840498@my.uvu.edu>
2020-09-03 15:24:31 -0600

    playing more with coding.

--- Cyborg Shrimp/Assets/Script/MoverScript.cs
+++ Cyborg Shrimp/Assets/Script/MoverScript.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MoverScript : MonoBehaviour
 {
-    public float speed = 3f;
-    public float x, y, z;
+    public float speed = 5f;
+    public int score = 100;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +17,17 @@ public class MoverScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        x = speed*Input.GetAxis("Horizontal")*Time.deltaTime;
-        y = speed*Input.GetAxis("Vertical")*Time.deltaTime;   
-        transform.Translate(x,y,z);
+        var vInput = speed * Time.deltaTime * Input.GetAxis("Vertical");
+        var hInput = speed * Time.deltaTime * Input.GetAxis("Horizontal");
+        transform.Translate(hInput,vInput,0);
+    }
+
+    public void Up()
+    {
+        transform.Translate(0,speed,0);
+    }
+    public void Down()
+    {
+        transform.Translate(0,-speed,0);
     }
 }
