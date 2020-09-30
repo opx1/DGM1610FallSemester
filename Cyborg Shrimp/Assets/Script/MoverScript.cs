550a25826d23fe21463d33c41e822fdb45bba8cf
Natalie Rust <10840498@my.uvu.edu>
2020-09-29 19:14:14 -0600

    9/15 Class catchup

--- Cyborg Shrimp/Assets/Script/MoverScript.cs
+++ Cyborg Shrimp/Assets/Script/MoverScript.cs
@@ -5,29 +5,20 @@ using UnityEngine;
 
 public class MoverScript : MonoBehaviour
 {
-    public float speed = 5f;
-    public int score = 100;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-    
-    }
+   public float moveSpeed;
+   private Vector3 moveDirection;
 
-    // Update is called once per frame
-    void Update()
-    {
-        var vInput = speed * Time.deltaTime * Input.GetAxis("Vertical");
-        var hInput = speed * Time.deltaTime * Input.GetAxis("Horizontal");
-        transform.Translate(hInput,vInput,0);
-    }
 
-    public void Up()
-    {
-        transform.Translate(0,speed,0);
-    }
-    public void Down()
-    {
-        transform.Translate(0,-speed,0);
-    }
+   public void Update()
+   {
+      if (Input.GetButton("Jump"))
+      {
+         moveDirection.x = moveSpeed * Time.deltaTime;
+      }
+      else
+      {
+         moveDirection.x = -moveSpeed * Time.deltaTime;
+      }
+      transform.Translate(moveDirection);
+   }
 }
