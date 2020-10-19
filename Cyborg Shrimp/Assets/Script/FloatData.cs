c8aadacfde0eaa30f82f661e838990f44d2100b1
Natalie Rust <10840498@my.uvu.edu>
2020-10-19 13:02:04 -0600

    kinda lost

---
+++ Cyborg Shrimp/Assets/Script/FloatData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatData : ScriptableObject
+{
+  public float value;
+
+  public void UpdateValue(float number)
+  {
+    value += number;
+  }
+}
