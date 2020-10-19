4b099193c9ffb966bb315b56503eb3c34cf608d5
Natalie Rust <10840498@my.uvu.edu>
2020-10-19 14:02:45 -0600

    9/17 Class catchup

--- Cyborg Shrimp/Assets/Script/PowerUp.cs
+++ Cyborg Shrimp/Assets/Script/PowerUp.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 public class PowerUp : MonoBehaviour
 {
     public int score;
+    public UnityEvent powerUpEvent;
     private void OnTriggerEnter(Collider other)
     {
         score++;
         print(score);
+        powerUpEvent.Invoke();
     }
 }
