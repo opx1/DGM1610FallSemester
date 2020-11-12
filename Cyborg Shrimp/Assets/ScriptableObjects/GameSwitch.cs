e8d85edf8e6dc61b120bd8f501703620df859ad4
Natalie Rust <10840498@my.uvu.edu>
2020-11-12 11:07:57 -0700

    11/10 class

---
+++ Cyborg Shrimp/Assets/ScriptableObjects/GameSwitch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+[CreateAssetMenu]
+public class GameSwitch : ScriptableObject
+{
+    public enum GameStates
+    {
+        Starting,
+        Playing,
+        Ending,
+        InStore,
+        Pausing
+    }
+
+    public GameStates currentGameState = GameStates.Starting;
+
+    public void ChangeToPlaying()
+    {
+        currentGameState = GameStates.Playing;
+    }
+    
+    public void RunCurrentState()
+    {
+        switch (currentGameState)
+        {
+            case GameStates.Starting:
+                Debug.Log("starting");
+                break;
+            case GameStates.Playing:
+                Debug.Log("playing");
+                break;
+            case GameStates.Ending:
+                Debug.Log("ending");
+                break;
+            case GameStates.InStore:
+                Debug.Log("instore");
+                break;
+            case GameStates.Pausing:
+                Debug.Log("pausing");
+                break;
+           
+        }
+    }
+        
+}
