2678bd2e585a0c0444775622b2cacc4220afd24b
Natalie Rust <10840498@my.uvu.edu>
2020-11-14 21:19:53 -0700

    10/27 39 mins

---
+++ Cyborg Shrimp/Assets/ScriptableObjects/Collectible.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class Collectible : ScriptableObject
+{
+  public float powerLevel = 1f;
+  public int cashValue = 10;
+  public Color artColor = Color.blue;
+  public Sprite art;
+}
