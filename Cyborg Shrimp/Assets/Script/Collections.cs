f68976e122775c18c894703def1173b4405f1371
Natalie Rust <10840498@my.uvu.edu>
2020-11-17 08:38:17 -0700

    10/27 class Loops and Lists

--- Cyborg Shrimp/Assets/Script/Collections.cs
+++ Cyborg Shrimp/Assets/Script/Collections.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class Collections : MonoBehaviour
 {
-   public string singleString;
-   public List<string> collectionList;
-   public List<GameObject> gameObjectList;
+    public List<Collectible> collectibleList;
 
-   private void Start()
-   {
-       foreach (var item in collectionList)
-       {
-          print(item);
-       }
+    private void Start()
+    {
+        for (var i = 0; i < collectibleList.Count; i++)
+        {
+            var position = new Vector3(x:i*2,0,0);
+            var item = collectibleList[i];
+            var newItem = new GameObject(item.name);
+            newItem.transform.position = position;
+            var sprite = newItem.AddComponent<SpriteRenderer>();
+            sprite.sprite = item.art;
+            sprite.color = item.artColor;
+        }
 
-       foreach (var item in gameObjectList)
-       {
-           print(item);
-       }
-   }
+    }
 }
