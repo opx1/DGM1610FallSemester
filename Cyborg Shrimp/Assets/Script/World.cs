ae7a47d4376be18f44d812232cce650939427b8d
Natalie Rust <10840498@my.uvu.edu>
2020-12-03 21:28:57 -0700

    Learning Scene Changes for my final game.

---
+++ Cyborg Shrimp/Assets/Script/World.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : SceneController
+{
+    public Transform player;
+    public new void Start()
+    {
+        base.Start();
+
+        switch (prevScene)
+        {
+            case "Switch1":
+                player.position = new Vector3(-2.5f,1f,0f);
+                break;
+            case "Switch3":
+                player.position = new Vector3(2.5f,1f,0f);
+                break;
+            case "Switch2": 
+                player.position= new Vector3(2.5f, 1f, 0f);
+                break;
+            default:
+                player.position = new Vector3(-2.5f,1f,0f);
+                break;
+        }
+    }
+}
