e2582ff43c5936832c81457032665ebb076b7dd0
Natalie Rust <10840498@my.uvu.edu>
2020-12-09 23:44:34 -0700

    Figuring out spawn points.
    That was way to hard.

--- Cyborg Shrimp/Assets/Script/MonoEvents.cs
+++ Cyborg Shrimp/Assets/Script/MonoEvents.cs
@@ -1,12 +1,25 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 using  UnityEngine.Events;
 public class MonoEvents : MonoBehaviour
 {
-    public UnityEvent startEvent;
-    private void Start()
+    public UnityEvent startEvent, awakeEvent, onEnableEvent;
+    public float holdTime;
+
+    private void Awake()
     {
+        awakeEvent.Invoke();
+    }
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(holdTime);
         startEvent.Invoke();
     }
 
-   
+    private void OnEnable()
+    {
+        onEnableEvent.Invoke();
+    }
 }
---
+++ Cyborg Shrimp/Assets/ScriptableObjects/Vector3Data.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class Vector3Data : ScriptableObject
+{
+    public Vector3 value;
+
+    public void SetValueFromTransform(Transform obj)
+    {
+        value = obj.position;
+    }
+
+    public void SetTransformFromValue(Transform obj)
+    {
+        obj.position = value;
+    }
+}
